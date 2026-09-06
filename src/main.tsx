8945ef55ac80dd4374ae63f12439274c20bea8ef
Muhammed Alimi <muhammedalimi32@gmail.com>
2026-09-05 22:52:55 -0600

    Initialize analytics before app render

--- src/main.tsx
+++ src/main.tsx
@@ -10,50 +10,71 @@
 // )
  /*  Version 2 */
 
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import { BrowserRouter } from 'react-router-dom'
+// import App from './App.tsx'
+// import { initAnalytics } from './analytics'
+// // import "./styles/reset.css";
+// // import "./styles/variables.css";
+// // import "./styles/layout.css";
+// // import "./styles/typography.css";
+// // import "./styles/navbar.css";
+// // import "./styles/hero.css";
+// // import "./styles/featured.css";
+// // import "./styles/articles.css";
+// // import "./styles/artist.css";
+// // import "./styles/issue.css";
+// // import "./styles/submit.css";
+// // import "./styles/footer.css";
+// // import "./styles/animations.css";
+// // import "./styles/responsive.css";
+
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>,
+// )
+
+// initAnalytics()
+
+// /*  Version 3 */
+
+// // import React from 'react'
+// // import ReactDOM from 'react-dom/client'
+// // import { BrowserRouter } from 'react-router-dom'
+// // import App from './App'
+// // import { AccessibilityProvider } from './context/AccessibilityContext'
+
+// // ReactDOM.createRoot(document.getElementById('root')!).render(
+// //   <React.StrictMode>
+// //     <BrowserRouter>
+// //       <AccessibilityProvider>
+// //         <App />
+// //       </AccessibilityProvider>
+// //     </BrowserRouter>
+// //   </React.StrictMode>,
+// // )
+
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+
 import App from './App.tsx'
 import { initAnalytics } from './analytics'
-// import "./styles/reset.css";
-// import "./styles/variables.css";
-// import "./styles/layout.css";
-// import "./styles/typography.css";
-// import "./styles/navbar.css";
-// import "./styles/hero.css";
-// import "./styles/featured.css";
-// import "./styles/articles.css";
-// import "./styles/artist.css";
-// import "./styles/issue.css";
-// import "./styles/submit.css";
-// import "./styles/footer.css";
-// import "./styles/animations.css";
-// import "./styles/responsive.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// Initialize Google Analytics before pages mount
+initAnalytics()
+
+ReactDOM.createRoot(
+  document.getElementById('root')!
+).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
 )
-
-initAnalytics()
-
-/*  Version 3 */
-
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import { BrowserRouter } from 'react-router-dom'
-// import App from './App'
-// import { AccessibilityProvider } from './context/AccessibilityContext'
-
-// ReactDOM.createRoot(document.getElementById('root')!).render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <AccessibilityProvider>
-//         <App />
-//       </AccessibilityProvider>
-//     </BrowserRouter>
-//   </React.StrictMode>,
-// )
