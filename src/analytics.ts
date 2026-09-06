92650f46ce907917b68295ed3861f605b4918c6c
Muhammed Alimi <muhammedalimi32@gmail.com>
2026-09-05 23:18:40 -0600

    Fix GA4 data layer command format

--- src/analytics.ts
+++ src/analytics.ts
@@ -125,6 +125,56 @@
 
 
 
+// const GA_ID = 'G-PFY9RL5BTL'
+
+// declare global {
+//   interface Window {
+//     dataLayer: unknown[]
+//     gtag?: (...args: unknown[]) => void
+//   }
+// }
+
+// export function initAnalytics() {
+//   if (document.getElementById('google-analytics')) {
+//     return
+//   }
+
+//   window.dataLayer = window.dataLayer || []
+
+//   window.gtag = (...args: unknown[]) => {
+//     window.dataLayer.push(args)
+//   }
+
+//   window.gtag('js', new Date())
+//   window.gtag('config', GA_ID)
+
+//   const script = document.createElement('script')
+//   script.id = 'google-analytics'
+//   script.async = true
+//   script.src =
+//     `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`
+
+//   document.head.appendChild(script)
+// }
+
+// export function trackPageView(
+//   title: string,
+//   path?: string
+// ) {
+//   document.title = title
+
+//   window.gtag?.('event', 'page_view', {
+//     page_title: title,
+//     page_path:
+//       path ?? window.location.pathname,
+//     page_location:
+//       window.location.href,
+//   })
+// }
+
+
+
+
 const GA_ID = 'G-PFY9RL5BTL'
 
 declare global {
@@ -141,8 +191,8 @@ export function initAnalytics() {
 
   window.dataLayer = window.dataLayer || []
 
-  window.gtag = (...args: unknown[]) => {
-    window.dataLayer.push(args)
+  window.gtag = function (...args: unknown[]) {
+    window.dataLayer.push(arguments)
   }
 
   window.gtag('js', new Date())
