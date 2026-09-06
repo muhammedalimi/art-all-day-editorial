cd9608deb6befc09e9281de43dd587667ed4c41f
Muhammed Alimi <muhammedalimi32@gmail.com>
2026-09-05 23:07:31 -0600

    Restore GA4 automatic page tracking

--- src/analytics.ts
+++ src/analytics.ts
@@ -71,6 +71,60 @@
 //   })
 // }
 
+// const GA_ID = 'G-PFY9RL5BTL'
+
+// declare global {
+//   interface Window {
+//     dataLayer: unknown[]
+//     gtag?: (...args: unknown[]) => void
+//   }
+// }
+
+// export function initAnalytics() {
+//   if (document.getElementById('google-analytics')) {
+//     return
+//   }
+
+//   window.dataLayer = window.dataLayer || []
+
+//   window.gtag = (...args: unknown[]) => {
+//     window.dataLayer.push(args)
+//   }
+
+//   window.gtag('js', new Date())
+
+//   window.gtag('config', GA_ID, {
+//     send_page_view: false,
+//   })
+
+//   const script = document.createElement('script')
+//   script.id = 'google-analytics'
+//   script.async = true
+//   script.src =
+//     `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`
+
+//   document.head.appendChild(script)
+// }
+
+// export function trackPageView(
+//   title: string,
+//   path?: string
+// ) {
+//   document.title = title
+
+//   window.gtag?.('event', 'page_view', {
+//     page_title: title,
+//     page_path:
+//       path ?? window.location.pathname,
+//     page_location:
+//       window.location.href,
+//   })
+// }
+
+
+
+
+
 const GA_ID = 'G-PFY9RL5BTL'
 
 declare global {
@@ -92,10 +146,7 @@ export function initAnalytics() {
   }
 
   window.gtag('js', new Date())
-
-  window.gtag('config', GA_ID, {
-    send_page_view: false,
-  })
+  window.gtag('config', GA_ID)
 
   const script = document.createElement('script')
   script.id = 'google-analytics'
